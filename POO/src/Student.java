5e0791f570955e01faba51eb77a35ddf736a716c
Gabriela donato <146032843+GabsDonato@users.noreply.github.com>
2025-02-28 11:26:24 -0300

    exemplo de classes

---
+++ POO/src/Student.java
@@ -0,0 +1,49 @@
+import java.awt.Color;
+
+public class Student {
+    String name;
+    int age;
+    Color color;
+    Sex sex;
+
+    void eating(Food food){
+        System.out.println("O " + name + "Esta comendo biscoito");
+        
+
+    }
+
+    void drinking(Drink drink){
+        System.out.println("O " + name + "Está bebendo leite");
+
+    }
+
+
+    void running(){
+        System.out.println("O " + name + "Não está correndo pois está de barriga cheia");
+
+    }
+
+    public static class School {
+        public static void main(String[] args) throws Exception {
+            Student student1 = new Student();
+            student1.name = "John";
+            student1.age = 20;
+            student1.color = Color.FAIR;
+            student1.sex = Sex.MALE;
+            
+        }
+
+    }
+}
+
+enum Sex {
+    MALE, FEMALE
+}
+
+class Food {
+    // Food class implementation
+}
+
+class Drink {
+    // Drink class implementation
+}
